f638973208166ed77f662de7a7b0a5f36a088e26
jyo <jyo@1706>
2025-04-04 14:33:02 +0530

    update

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,20 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import Inspect from 'vite-plugin-inspect';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [Inspect()],
- build: {
-  chunkSizeWarningLimit: 1000,
+  plugins: [react()],
+  build: {
     rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'], // Put common libraries into a separate chunk
-        }
-      }
-    }
-  },
-    
+      external: ['react-router-dom'],
+    },
+   
 }
-)
+})
