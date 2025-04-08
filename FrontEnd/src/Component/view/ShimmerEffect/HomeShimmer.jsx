f05ee834fa87b9abb3c572c5c4a01715b1d94a1e
jyo <93206041+jyo1706@users.noreply.github.com>
2025-04-08 11:55:45 +0530

    update

--- FrontEnd/src/Component/view/ShimmerEffect/HomeShimmer.jsx
+++ FrontEnd/src/Component/view/ShimmerEffect/HomeShimmer.jsx
@@ -1,43 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import '../../assets/HomeShimmer.css'
 function HomeShimmer() {
   return (
 
 <div className='container'>
-     <div className="homeCardContainer">
-    <div className="product-not-found">
-        <Link>
-            <div className='homeCard'>
-                <div className='text-center'>
-                         
-                </div>
-                <div className='mb-1 cardDetail'>
-                    <span><h6 className='cardHeading'></h6></span>
-                        <div className='cardPrice'> 
-                              <span></span>
-                              <span></span>
-                              <span></span>
-                        </div>
-
-                        <div></div>
-                         <div className='review' style={{color:"green",marginRight:"10px"}}>
-                         </div>
-                      </div>
-                     
-             </div>
-             </Link>
-        
-              
-              
-       
-        
-            
-
-           
-          
-     </div>
-     </div>
+    <div className="homeCardContainer shimmercard"></div>
+    <div className="homeCardContainer shimmercard"></div>
+    <div className="homeCardContainer shimmercard"></div>
+    <div className="homeCardContainer shimmercard"></div>
+    <div className="homeCardContainer shimmercard"></div>
+    <div className="homeCardContainer shimmercard"></div>
+    <div className="homeCardContainer shimmercard"></div>
+    <div className="homeCardContainer shimmercard"></div>
+    <div className="homeCardContainer shimmercard"></div>
+    <div className="homeCardContainer shimmercard"></div>
+    <div className="homeCardContainer shimmercard"></div>
+    <div className="homeCardContainer shimmercard"></div>
+    <div className="homeCardContainer shimmercard"></div>
+    <div className="homeCardContainer shimmercard"></div>
+    <div className="homeCardContainer shimmercard"></div>
 </div>
   )
 }
