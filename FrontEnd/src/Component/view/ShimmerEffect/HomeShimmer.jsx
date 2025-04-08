ca3f76ebb7aab86c49192de3615382a7fc260a47
jyo <93206041+jyo1706@users.noreply.github.com>
2025-04-08 13:05:16 +0530

    Update HomeShimmer.jsx

--- FrontEnd/src/Component/view/ShimmerEffect/HomeShimmer.jsx
+++ FrontEnd/src/Component/view/ShimmerEffect/HomeShimmer.jsx
@@ -1,26 +1,31 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import '../../../assets/HomeShimmer.css'
+import '../../../assets/homeShimmer.css'
 function HomeShimmer() {
-  return (
 
-<div className='container'>
-    <div className="homeCardContainer shimmercard"><div className='homeCard'></div></div>
-    <div className="homeCardContainer shimmercard"><div className='homeCard'></div></div>
-    <div className="homeCardContainer shimmercard"><div className='homeCard'></div></div>
-    <div className="homeCardContainer shimmercard"><div className='homeCard'></div></div>
-    <div className="homeCardContainer shimmercard"><div className='homeCard'></div></div>
-    <div className="homeCardContainer shimmercard"><div className='homeCard'></div></div>
-    <div className="homeCardContainer shimmercard"><div className='homeCard'></div></div>
-    <div className="homeCardContainer shimmercard"><div className='homeCard'></div></div>
-    <div className="homeCardContainer shimmercard"><div className='homeCard'></div></div>
-    <div className="homeCardContainer shimmercard"><div className='homeCard'></div></div>
-    <div className="homeCardContainer shimmercard"><div className='homeCard'></div></div>
-    <div className="homeCardContainer shimmercard"><div className='homeCard'></div></div>
-    <div className="homeCardContainer shimmercard"><div className='homeCard'></div></div>
-    <div className="homeCardContainer shimmercard"><div className='homeCard'></div></div>
-    <div className="homeCardContainer shimmercard"><div className='homeCard'></div></div>
-</div>
+    const mapped = Array.from({length:10}).map((val,i)=>
+        {
+            return <div key={i} className='homeCard shimmerCard'>
+                    <div className='text-center h-75'> </div>
+                    <div className='mb-1 cardDetail'><span><h6 className='cardHeading'></h6></span>
+                    <div className='cardHeading ShimmerHeading'></div>
+                    <div className='cardPrice ShimmerPrice'>
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                    </div>
+                    <div className='review ShimmerReview'></div>
+                 </div>
+            </div>
+      })
+     
+  return (
+    <div>
+        <div className='container'>
+            <div className='homeCardContainer'>
+                 {mapped}
+            </div>
+        </div>
+    </div>
   )
 }
 
